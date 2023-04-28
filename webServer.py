689cf96fb29611fe5b512a53c5c2c33a4acc9aff
manuelbarral <manuel.barral.2002@gmail.com>
2023-04-28 16:51:36 +0100

    weather table now takes (date, location) as a key

--- webServer.py
+++ webServer.py
@@ -19,8 +19,8 @@ def connectDB(dbname):
     cursor = connection.cursor()
     if not dbIsCreated:
         cursor.execute("PRAGMA foreign_keys = ON;") # Allow foreign keys
-        cursor.execute("CREATE TABLE weather (id INTEGER PRIMARY KEY, date INTEGER, location TEXT,\
-                        condition TEXT, mintemp_c INTEGER, maxtemp_c INTEGER);") # Create table weather
+        cursor.execute("CREATE TABLE weather (date INTEGER, location TEXT, condition TEXT, mintemp_c INTEGER, maxtemp_c INTEGER, \
+                        PRIMARY KEY(date, location);") # Create table weather with key - (date,location)
         cursor.execute("CREATE TABLE locations (id INTEGER, name TEXT, IATA TEXT, wea_name TEXT);") # Criação da tabela locations 
         cursor.execute("CREATE TABLE legs (id TEXT, dep_IATA TEXT, arr_IATA TEXT, dep_datetime INTEGER, arr_datetime INTEGER,\
                         duration_min INTEGER, arlineCodes TEXT)") # Criação da tabela legs (voos)
@@ -66,7 +66,8 @@ def search(location=None, cost=None):
             req= requests.get(url)
             if req.content != b'Cidade nao encontrada':
                 print(json.loads(req.content)['location']['name'])
-                
+                # Populate the table weather (we dont need id for this table as long as date and location are a unique pair of keysd)
+
             else:
                 print(city, ": Não foi Encontrada")
         r = make_response(jsonify("Placeholder for response to search: viagens (roundtrips) from location to another under price stipulated"))
